6474688a434ecdeb7cfd443724e3fc280018add5
Al Aji <alajiseno@gmail.com>
2015-08-09 20:30:40 +0700

    comment review

--- application/views/vw_detail.php
+++ application/views/vw_detail.php
@@ -83,68 +83,74 @@
 	?>
     
 	<div class="wrapper" id="content">
-	
-	<div class="row">
-	<div id="map-canvas">
-	</div>
-	<div id="details">
-		<?php
-		//echo $_SESSION['username'];
-		echo "Name\t: ". $xml->result->{'item' .$_GET['id']}->name . "<br>";
-		echo "Longitude\t: ". $xml->result->{'item' .$_GET['id']}->longitude. "<br>";
-		echo "Latitude\t: ". $xml->result->{'item' .$_GET['id']}->latitude. "<br>";
-		echo "Village\t: ". $xml->result->{'item' .$_GET['id']}->village. "<br>";
-		echo "Subdistrict\t: ". $xml->result->{'item' .$_GET['id']}->subdistrict. "<br>";
-		echo "District\t: ". $xml->result->{'item' .$_GET['id']}->district. "<br>";
-		echo "Address\t: ". $xml->result->{'item' .$_GET['id']}->address. "<br>";
-		echo "Telephone\t: ". $xml->result->{'item' .$_GET['id']}->telephone. "<br>";
-		?>
-	</div>
-	<div id="usermenu">
-	<div id="sharebutton">
-	<span class='st_facebook_large'></span>
-	<span class='st_twitter_large'></span>
-	<span class='st_linkedin_large'></span>
-	<span class='st_pinterest_large'></span>
-	<span class='st_whatsapp_large' ></span>
-	<span class='st_email_large'></span>
-	</div>
-
-	<div>
-		<form method="post" action="detail/input"> 
-			<input type="textarea" name="comment" placeholder="Your comment here...">
-			<input type="hidden" name="user" value="<?php echo $_SESSION['username']?>">
-			<input type="hidden" name="destination" value="<?php echo $xml->result->{'item' .$_GET['id']}->name?>">
-			<form id="ratingsForm">
-				<div class="stars">
-					<span>
-					<input type="radio" name="star" class="star-1" id="star-1" />
-					<label class="star-1" for="star-1"><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img></label>
-					<br>
-					<input type="radio" name="star" class="star-2" id="star-2" />
-					<label class="star-2" for="star-2"><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img></label>
-					<br>
-					<input type="radio" name="star" class="star-3" id="star-3" />
-					<label class="star-3" for="star-3"><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img></label>
-					<br>
-					<input type="radio" name="star" class="star-4" id="star-4" />
-					<label class="star-4" for="star-4"><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img></label>
-					<br>
-					<input type="radio" name="star" class="star-5" id="star-5" />
-					<label class="star-5" for="star-5"><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img></label>
-					</span>
+	<div class="content-wrapper">
+        <div class="container">
+        	<br>
+        	<div class="box box-default">
+              <div class="box-header with-border">
+				<div class="row">
+				<div id="map-canvas">
 				</div>
-			</form>
-			<!--<input type="hidden" name="rate" value="">-->
-			<!--<input type="hidden" name="time" value="">-->
-			<input type="submit" value="Comment" name="input"/>
-		</form>
-	</div>
-	</div>
-	</div>
+				<div id="details">
+					<?php
+					//echo $_SESSION['username'];
+					echo "Name\t: ". $xml->result->{'item' .$_GET['id']}->name . "<br>";
+					echo "Longitude\t: ". $xml->result->{'item' .$_GET['id']}->longitude. "<br>";
+					echo "Latitude\t: ". $xml->result->{'item' .$_GET['id']}->latitude. "<br>";
+					echo "Village\t: ". $xml->result->{'item' .$_GET['id']}->village. "<br>";
+					echo "Subdistrict\t: ". $xml->result->{'item' .$_GET['id']}->subdistrict. "<br>";
+					echo "District\t: ". $xml->result->{'item' .$_GET['id']}->district. "<br>";
+					echo "Address\t: ". $xml->result->{'item' .$_GET['id']}->address. "<br>";
+					echo "Telephone\t: ". $xml->result->{'item' .$_GET['id']}->telephone. "<br>";
+					?>
+				</div>
+				<div id="usermenu">
+				<div id="sharebutton">
+				<span class='st_facebook_large'></span>
+				<span class='st_twitter_large'></span>
+				<span class='st_linkedin_large'></span>
+				<span class='st_pinterest_large'></span>
+				<span class='st_whatsapp_large' ></span>
+				<span class='st_email_large'></span>
+				</div>
 
-	<div>instagram feed</div>
-	<div id="instafeed">	
+				<div>
+					<form method="post" action="detail/input"> 
+						<input type="textarea" name="comment" placeholder="Your comment here...">
+						<input type="hidden" name="user" value="<?php echo $_SESSION['username']?>">
+						<input type="hidden" name="destination" value="<?php echo $xml->result->{'item' .$_GET['id']}->name?>">
+							<div class="stars">
+								<span>
+								<input type="radio" name="rate" class="star-1" id="star-1" value=1/>
+								<label class="star-1" for="star-1"><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img></label>
+								<br>
+								<input type="radio" name="rate" class="star-2" id="star-2" value=2>
+								<label class="star-2" for="star-2"><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img></label>
+								<br>
+								<input type="radio" name="rate" class="star-3" id="star-3" value=3/>
+								<label class="star-3" for="star-3"><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img></label>
+								<br>
+								<input type="radio" name="rate" class="star-4" id="star-4" value=4/>
+								<label class="star-4" for="star-4"><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img></label>
+								<br>
+								<input type="radio" name="rate" class="star-5" id="star-5" value=5/>
+								<label class="star-5" for="star-5"><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img><img id="logo" height="20px" src="<?php echo base_url('') ?>image/star.png"></img></label>
+								</span>
+							</div>
+						<!--<input type="hidden" name="rate" value="">-->
+						<!--<input type="hidden" name="time" value="">-->
+						<input type="submit" value="Comment" name="input"/>
+					</form>
+				</div>
+				</div>
+				</div>
+
+				<div>instagram feed</div>
+				<div id="instafeed">	
+				</div>
+			</div>
+		</div>
+		</div>
 	</div>
 	
 	<?php $this->load->view('include/footer');
